82fac7b69930e8259ea7341e109533346f8a04a9
HenriqueLopes-dev <ohenriquelopes.dev@gmail.com>
2025-10-31 08:15:21 -0300

    adicionando projeto inicial

---
+++ src/test/java/io/github/HenriqueLopes_dev/BackendFortniteApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.HenriqueLopes_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendFortniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
