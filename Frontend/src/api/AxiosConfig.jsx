383f63f6a722c7260f1b293f79e868715ed3d612
HenriqueLopes-dev <ohenriquelopes.dev@gmail.com>
2025-11-18 18:11:37 -0300

    tentando resolver error CORS

--- Frontend/src/api/AxiosConfig.jsx
+++ Frontend/src/api/AxiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://fullstackfortnite.onrender.com/",
+  baseURL: "https://fullstackfortnite.onrender.com",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
